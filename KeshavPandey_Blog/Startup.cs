149ebd673529d42a01bdd6102b6f37494c9e09b1
Keshav Pandey <Keshav25@outlook.com>
2017-07-05 00:01:23 +0530

    Commit to start it all over again

---
+++ KeshavPandey_Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KeshavPandey_Blog.Startup))]
+namespace KeshavPandey_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
